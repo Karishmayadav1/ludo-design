6629f5c63fa3bb4b08c1c3fa5844981009a84bc2
karishma yadav <karishmay353@gmail.com>
2023-07-16 15:32:07 +0530

    first commit

---
+++ src/Component/Board.js
@@ -0,0 +1,234 @@
+import React from "react";
+
+function Board(){
+    return(
+        <>
+        
+        <div className="outer">
+            <div className="box_row">
+              <div className="box border_red">
+                <div className="circle red"></div>
+                <div className="circle red"></div>
+                <div className="circle red"></div>
+                <div className="circle red"></div>
+              </div>
+           
+              
+
+        <div className="gap">
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/21/21178.png"/></div>
+                <div className="row"></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row green"></div>
+                <div className="row green"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+            </div>
+            <div className="gap_row">            
+                <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                <div className="row green"></div>
+                <div className="row "></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row green"></div>
+                <div className="row "></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row green"></div>
+                <div className="row "></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row green"></div>
+                <div className="row "></div>
+            </div>
+        </div>     
+
+        
+              <div className="box border_green">
+                <div className="circle green"></div>
+                <div className="circle green"></div>
+                <div className="circle green"></div>
+                <div className="circle green"></div>
+             </div>
+        </div>
+        <div className="middle_row">
+            <div className="A1">
+                <div className="A1_row">
+                    <div className="row"></div>
+                    <div className="row red"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                </div>
+                
+                <div className="A1_row">
+                    <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/2958/2958791.png"/></div>
+                    <div className="row red"></div>
+                    <div className="row red"></div>
+                    <div className="row red"></div>
+                    <div className="row red"></div>
+                    <div className="row red"></div>
+                </div>
+
+                <div className="A1_row">
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                </div> 
+            </div>
+
+            <div className="ludo_home"></div>
+            <div className="A1">
+                <div className="A1_row">
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                </div>
+                
+                <div className="A1_row">
+                    <div className="row yellow"></div>
+                    <div className="row yellow"></div>
+                    <div className="row yellow"></div>
+                    <div className="row yellow"></div>
+                    <div className="row yellow"></div>
+                    <div className="row"><img src="https://w7.pngwing.com/pngs/531/63/png-transparent-black-arrow-illustration-black-white-pattern-arc-arrow-angle-white-text-thumbnail.png"/></div>
+                </div>
+
+                <div className="A1_row">
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+                    <div className="row yellow"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                    <div className="row"></div>
+                </div> 
+                </div>
+
+
+             <div className="box_row">
+              <div className="box border_blue">
+                <div className="circle blue"></div>
+                <div className="circle blue"></div>
+                <div className="circle blue"></div>
+                <div className="circle blue"></div>
+              </div>
+           
+              
+
+        <div className="gap">
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row blue"></div>
+                <div className="row"></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row blue"></div>
+                <div className="row"></div>
+            </div>
+            <div className="gap_row">            
+                <div className="row"></div>
+                <div className="row blue"></div>
+                <div className="row "></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row blue"></div>
+                <div className="row"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+            </div>
+            <div className="gap_row">
+                <div className="row blue"><img src="https://cdn-icons-png.flaticon.com/512/1828/1828884.png"/></div>
+                <div className="row blue"></div>
+                <div className="row"></div>
+            </div>
+            <div className="gap_row">
+                <div className="row"></div>
+                <div className="row"><img src="https://w7.pngwing.com/pngs/824/363/png-transparent-black-arrow-illustration-arrow-arc-arrow-angle-triangle-3d-arrows-thumbnail.png"/></div>
+                <div className="row"></div>
+            </div>
+        </div>     
+
+        
+              <div className="box border_yellow">
+                <div className="circle yellow"></div>
+                <div className="circle yellow"></div>
+                <div className="circle yellow"></div>
+                <div className="circle yellow"></div>
+
+                
+             </div>
+             
+        </div>
+
+         </div>
+        
+       
+
+
+
+         </div>
+
+
+    
+            
+       
+
+
+        </>
+    )
+}
+   export default Board;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
